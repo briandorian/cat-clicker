18d3d8de152ae7bde106de619777baec26675de0
Brian Acosta <brian.acosta@xing.com>
2019-02-06 13:01:24 +0100

    Adding two cats and two counters plus the name above each of them

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
-let counter=0;
-const elem = document.getElementById('cat');
-elem.addEventListener('click', function(){
+let first_counter=0;
+let second_counter=0;
+const first_cat = document.getElementById('cat');
+const second_cat = document.getElementById('second_cat');
+first_cat.addEventListener('click', function(){
+  catClicked();
+}, false);
+second_cat.addEventListener('click', function(){
   catClicked();
 }, false);
 
 function catClicked (){
-  const elementClicked = event.target;
   updateCounter();
 }
 
 function updateCounter(){
-    counter++;
-    document.getElementById("counter-clicks").innerHTML = counter + " Clicks";
-}
+    if (event.target.id == "cat"){
+      first_counter++;
+      document.getElementById("counter-clicks_kitty").innerHTML = first_counter + " Clicks";
+    }
+    else if (event.target.id == "second_cat"){
+      second_counter++;
+      document.getElementById("second_counter-clicks_missy").innerHTML = second_counter + " Clicks";
+    }
+  }
